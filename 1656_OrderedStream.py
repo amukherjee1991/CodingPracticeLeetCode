9de5d90902a9558417cba7603ea13579640f38d0
Arghya Mukherjee <39413425+amukherjee1991@users.noreply.github.com>
2023-03-26 15:03:49 -0500

    comments added

--- 1656_OrderedStream.py
+++ 1656_OrderedStream.py
@@ -4,6 +4,7 @@ class OrderedStream(object):
         """
         :type n: int
         """
+         # Initialize the OrderedStream with an empty buffer of size n
         self.n = n
         self.ptr = 0
         self.buffer = [None] * n
@@ -14,10 +15,17 @@ class OrderedStream(object):
         :type value: str
         :rtype: List[str]
         """
+        # Subtract 1 from idKey to convert from 1-indexing to 0-indexing
         idKey -= 1
+        # Insert the value into the buffer at the correct index
         self.buffer[idKey] = value
+        # If the inserted idKey is not equal to the current pointer ptr,
+        # then return an empty list since we don't have a contiguous chunk
         if idKey != self.ptr:
             return []
+        # If the inserted idKey is equal to ptr, find the next contiguous
+        # chunk of non-None values in the buffer
         while self.ptr < self.n and self.buffer[self.ptr] is not None:
             self.ptr += 1
+        # Return the contiguous chunk from idKey up to ptr
         return self.buffer[idKey:self.ptr]
